ecdee93cdd1f9cf33eac7045ea3faa236e7b2e50
zahidyew <zahidyew96@gmail.com>
2020-04-23 10:00:27 +0800

    added dotenv

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const Unsplash = require('unsplash-js').default;
 const toJson = require('unsplash-js').toJson;
-const keys = require('./Keys.js');
+require('dotenv').config();
 
 // init an express obj so that we can access express funcs thru app.
 const app = express();
@@ -16,7 +16,7 @@ app.use(cors({
 
 // init an unsplash obj with the api access keys for authentication purpose
 const unsplash = new Unsplash({
-   accessKey: keys.accessKey
+   accessKey: process.env.API_KEY
 });
 
 // creating routes/endpoints for the server
@@ -34,10 +34,6 @@ app.get('/api/getPhotos', (req, res) => {
       .then(toJson)
       .then(json => res.json(json));
 });
-
-//TO-DO later
-//get a list of pictures satisfying a tag
-//pick a picture randomly 
 
 // define port for the server
 const PORT = process.env.PORT || 5000;
